0919005e8daa0553747d59c0baf778bfef7cfbce
Jivaros <jivarosgbeto@gmail.com>
2023-01-31 17:04:34 +0100

    feat: app database ddl

--- dev-env/benindev.ddl.sql
+++ dev-env/benindev.ddl.sql
@@ -1,5 +1,44 @@
-create table
-    Account(
-        email varchar(100) not null primary key,
-        password varchar(200)
-    );
+
+
+drop table if exists Photo;
+drop table if exists Gratuation;
+drop table if exists Profil;
+drop table if exists Account;
+
+create table Account(
+  id integer not null primary key autoincrement,
+  email varchar(100) not null unique,
+  passwordHash varchar(200)
+);
+
+create table Profil(
+  accountId integer not null references Account (id),
+  pseudo varchar(50),
+  firstName varchar(100),
+  lastName varchar(100),
+  skills json,
+  localisation varchar(200),
+  description text,
+  links json,
+  createAt timestamp not null default current_timestamp,
+  updateAt timestamp not null default current_timestamp,
+  primary key (accountId)
+);
+
+create table Gratuation(
+  profilId integer not null references Profil (accountId),
+  status varchar,
+  gatuationYear integer,
+  primary key (profilId)
+);
+
+create table Photo(
+  uid varchar not null primary key,
+  originalName varchar not null,
+  height integer not null,
+  width integer not null,
+  weightB integer not null,
+  mediaType varchar(50) not null,
+  binaryFile blob not null,
+  profilId integer not null references Profil (accountId)
+);
